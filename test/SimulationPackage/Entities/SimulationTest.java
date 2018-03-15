58ca2a189d0633c1dbc752e13740264354bd5f9b
xprovoke <mikela@outlook.dk>
2018-03-15 10:06:50 +0100

    Minor change

--- test/SimulationPackage/Entities/SimulationTest.java
+++ test/SimulationPackage/Entities/SimulationTest.java
@@ -1,4 +1,16 @@
 package SimulationPackage.Entities;
 
+import GFX.Display;
+import SimulationPackage.Simulation;
+import org.junit.jupiter.api.BeforeEach;
+
 public class SimulationTest {
+
+    Simulation simulation;
+    Display display;
+
+    @BeforeEach
+    public void beforeEach() {
+        
+    }
 }
